bf87b26253b37d1be2f53c2eb8d267727e2f0cab
Thomas Mailund <mailund@birc.au.dk>
2022-11-27 11:30:42 +0100

    experimenting with slices

--- Chapter-01-Strings-in-C/test_test.c
+++ Chapter-01-Strings-in-C/test_test.c
@@ -3,4 +3,27 @@
 
 #include <utest.h>
 
-UT_SUITE(UT_ERROR_IF(2 == 2))
+// clang-format off
+#define slice_type(T_)             struct { T_ *buf; size_t len; }
+#define slice_from_array(A_, LEN_) { .buf = A_, .len = LEN_ }
+#define slice(S_, I_, J_)          { .buf = S_.buf + I_, .len = J_ - I_ }
+// clang-format on
+
+typedef slice_type(int) int_slice;
+static void print_int_slice(int_slice slice)
+{
+    for (size_t i = 0; i < slice.len; i++)
+    {
+        printf("%d\n", slice.buf[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    int arr[] = {1, 2, 3};
+    int_slice slice = slice_from_array(arr, sizeof arr);
+    int_slice suf1 = slice(slice, 1, slice.len);
+    print_int_slice(suf1);
+    return 0;
+}
