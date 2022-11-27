6d62379138a93d79941d4297ed4d8d7e68512e05
Thomas Mailund <mailund@birc.au.dk>
2022-11-27 18:23:58 +0100

    worthless playing

--- Chapter-01-Strings-in-C/test_test.c
+++ Chapter-01-Strings-in-C/test_test.c
@@ -4,9 +4,9 @@
 #include <utest.h>
 
 // clang-format off
-#define slice_type(T_)             struct { T_ *buf; size_t len; }
+#define slice_type(T_) struct      { T_ *buf; size_t len; }
 #define slice_from_array(A_, LEN_) { .buf = A_, .len = LEN_ }
-#define slice(S_, I_, J_)          { .buf = S_.buf + I_, .len = J_ - I_ }
+#define sub(S_, I_, J_)            { .buf = S_.buf + I_, .len = J_ - I_ }
 // clang-format on
 
 typedef slice_type(int) int_slice;
@@ -19,11 +19,58 @@ static void print_int_slice(int_slice slice)
     printf("\n");
 }
 
+struct match_iter;
+struct match_iter_type
+{
+    void *(*alloc)(const char *x, const char *p);
+    void (*dealloc)(struct match_iter *itr);
+    int (*next)(struct match_iter *itr);
+};
+struct match_iter
+{
+    struct match_iter_type *type;
+};
+
+void new_match_iter(const char *x, const char *p, struct match_iter_type *type)
+{
+    struct match_iter *itr = type->alloc(x, p);
+}
+
+void free_match_iter(struct match_iter *itr)
+{
+    itr->type->dealloc(itr);
+}
+
+void init_match_iter(struct match_iter *itr,
+                     struct match_iter_type *type,
+                     const char *x, const char *p)
+{
+    itr->type = type;
+    itr->x = x;
+    itr->p = p;
+}
+
+int next(struct match_iter *itr)
+{
+    return itr->type->next(itr);
+}
+
+void dealloc(struct match_iter *itr)
+{
+    itr->type->dealloc(itr);
+}
+
+struct naive_match_iter
+{
+    struct match_iter_type *vtable;
+    const char *x, *p;
+};
+
 int main(void)
 {
     int arr[] = {1, 2, 3};
-    int_slice slice = slice_from_array(arr, sizeof arr);
-    int_slice suf1 = slice(slice, 1, slice.len);
+    int_slice x = slice_from_array(arr, sizeof arr);
+    int_slice suf1 = sub(x, 1, x.len);
     print_int_slice(suf1);
     return 0;
 }
